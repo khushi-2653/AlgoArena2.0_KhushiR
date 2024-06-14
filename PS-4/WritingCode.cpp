0f11bf0874e4365978cf7e247d9b15a1e9133045
khushi-2653 <138553904+khushi-2653@users.noreply.github.com>
2024-06-14 19:02:48 +0530

    Update WritingCode.cpp

--- PS-4/WritingCode.cpp
+++ PS-4/WritingCode.cpp
@@ -1,12 +1,4 @@
 #include <bits/stdc++.h>
-
-// #include <ext/pb_ds/assoc_container.hpp> 
-// #include <ext/pb_ds/tree_policy.hpp> 
-// using namespace __gnu_pbds; 
-// #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-// #define ordered_multiset2 tree<int, null_type,less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>
-// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
-
 #define pb push_back
 #define int int64_t
 #define loop(n) for(int i = 0 ; i < n ; i++)
